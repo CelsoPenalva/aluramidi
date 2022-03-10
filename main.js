50516c61fb020345d0092643d9ba0016098f36e7
Celso Neto <adm.celsoneto@gmail.com>
2022-03-09 23:00:14 -0300

    Concluindo o Curso de JavaScript para Web

---
+++ main.js
@@ -0,0 +1,65 @@
+function tocaSom(seletorAudio) {
+
+   const elemento = document.querySelector(seletorAudio);
+     
+      //if (elemento != null && elemento.localName === 'audio') =
+   if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+
+   }
+   else {
+       //alert('Elemento não encontrado');
+       console.log('Elemento não encontrado ou seletor inválido');
+   }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}` // template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+
+
+}
+
+
+
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+
+//     // template string
+//     const idAudio = `#som_${instrumento}`
+
+//     //console.log(idAudio);
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     };
+   
+//     contador = contador + 1;
+
+//     //console.log(contador);
+// }
